20d84404e9efa70acf088a2c45b005242060a067
userPc <orgimeno@gmail.com>
2019-01-28 00:59:37 +0100

    Terminar parte de la API

--- src/AppBundle/Controller/CommentController.php
+++ src/AppBundle/Controller/CommentController.php
@@ -2,16 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use BackendBundle\Entity\Comment;
 use BackendBundle\Entity\User;
+use BackendBundle\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class CommentController extends Controller
 {
 
     /**
-     * Create Video
+     * Create Comment
      *
      * @throws \Exception
      */
@@ -30,42 +31,47 @@ class CommentController extends Controller
                 $params = json_decode($json);
 
                 $created_at = new \DateTime('now');
-                $update_at = new \DateTime('now');
-                $imagen = null;
-                $videos_path = null;
 
                 $user_id =($identity->sub != null) ? $identity->sub : null;
-                $title = (isset($params->title)) ? $params->title : null;
-                $description = (isset($params->description )) ? $params->description : null;
-                $status = (isset($params->status )) ? $params->status : null;
+                $video_id = (isset($params->video_id)) ? $params->video_id : null;
+                $body = (isset($params->body )) ? $params->body : null;
 
-                if($user_id != null && $title != null){
+                if($user_id != null && $video_id != null){
                     /** @var User $user */
                     $user = $em->getRepository(User::class)->find($identity->sub);
 
+                    $video = $em->getRepository(Video::class)->findOneBy(['id' => $video_id]);
 
-                    $video = new Video();
+                    if(is_object($video)){
 
-                    $video->setCreatedAt($created_at);
-                    $video->setUpdatedAt($update_at);
-                    $video->setUser($user);
-                    $video->setDescription($description);
-                    $video->setStatus($status);
-                    $video->setTitle($title);
+                        $comment = new Comment();
 
-                    $em->persist($video);
-                    $em->flush();
+                        $comment->setCreatedAt($created_at);
+                        $comment->setUser($user);
+                        /** @var Video $video */
+                        $comment->setVideo($video);
+                        $comment->setBody($body);
 
-                    $data = [
-                        'status' => "success",
-                        'code' => 200,
-                        'data' => $video
-                    ];
+                        $em->persist($comment);
+                        $em->flush();
+
+                        $data = [
+                            'status' => "success",
+                            'code' => 200,
+                            'data' => $comment
+                        ];
+                    }else{
+                        $data = [
+                            'status' => "error",
+                            'code' => 404,
+                            'msg' => "Video no encontrado"
+                        ];
+                    }
                 }else{
                     $data = [
                         'status' => "error",
                         'code' => 460,
-                        'msg' => "Video not created"
+                        'msg' => "Comment not created"
                     ];
                 }
 
@@ -73,7 +79,7 @@ class CommentController extends Controller
                 $data = [
                     'status' => "error",
                     'code' => 470,
-                    'msg' => "Params failed, video not created"
+                    'msg' => "Params failed, comment not created"
                 ];
             }
 
@@ -82,6 +88,94 @@ class CommentController extends Controller
                 'status' => "error",
                 'code' => 400,
                 'msg' => "Authorazation not valid"
+            ];
+        }
+
+        return $helpers->json($data);
+    }
+
+    /**
+     * Delete Comment
+     *
+     * @throws \Exception
+     */
+    public function deleteAction(Request $request, $id = null){
+        $helpers = $this->get('app.helpers');
+        $hash = $request->get('authorization', null);
+        $authCheck = $helpers->authCheck($hash);
+
+        if($authCheck){
+            $em = $this->getDoctrine()->getManager();
+            $identity = $helpers->authCheck($hash, true);
+
+            $user_id =($identity->sub != null) ? $identity->sub : null;
+            $comment = $em->getRepository(Comment::class)->find($id);
+
+            if(isset($user_id) &&
+                ($user_id == $comment->getUser()->getId() ||
+                    $user_id == $comment->getVideo()->getUser->getId())){
+
+                $em->remove($comment);
+                $em->flush();
+
+                $data = [
+                    'status' => "success",
+                    'code' => 200,
+                    'msg' => "Borrado correctamente"
+                ];
+            }
+        }else{
+            $data = [
+                'status' => "error",
+                'code' => 400,
+                'msg' => "Authorazation not valid"
+            ];
+        }
+
+        return $helpers->json($data);
+    }
+
+    /**
+     * List Comments
+     *
+     * @param null $id
+     * @return
+     */
+    public function listAction($id = null){
+        $helpers = $this->get('app.helpers');
+
+        $em = $this->getDoctrine()->getManager();
+
+        if($id != null){
+
+            $video = $em->getRepository(Video::class)->find($id);
+
+            $comments = $em->getRepository(Comment::class)->findBy([
+                'video' => $video
+            ],
+            [
+                'id' => "desc"
+            ]);
+
+            if(count($comments) >= 1){
+                $data = [
+                    'status' => "success",
+                    'code' => 200,
+                    'data' => $comments
+                ];
+            }else{
+                $data = [
+                    'status' => "error",
+                    'code' => 402,
+                    'msg' => "No comments found in this video"
+                ];
+            }
+
+        }else{
+            $data = [
+                'status' => "error",
+                'code' => 402,
+                'msg' => "Videos not found"
             ];
         }
 
