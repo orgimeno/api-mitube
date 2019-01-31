f2c67b74b653d43a4bf1a87c964bf785cd49fe9d
Oscar Rodriguez Gimeno <orgimeno@gmail.com>
2019-01-31 10:07:35 +0100

    Fix typo with CORS headers

--- web/app.php
+++ web/app.php
@@ -5,8 +5,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PT, DELETE");
-header("Allow: GET, POST, OPTIONS, PT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
 $method = $_SERVER["REQUEST_METHOD"];
 if($method == "OPTIONS"){
